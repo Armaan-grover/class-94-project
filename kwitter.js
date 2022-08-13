a9ef4b0a9cc636ec57673bb07dc5d87c9eaf0eb9
Armaan-grover <97002933+Armaan-grover@users.noreply.github.com>
2022-08-13 07:00:11 -0500

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -2,6 +2,6 @@ function addUser(){
 
     var user=document.getElementById("nameInput").value;
     localStorage.setItem("user",user);
-    window.location="kwitter_room.html";
+    window.location="kwitterRoom.html";
     }
     
